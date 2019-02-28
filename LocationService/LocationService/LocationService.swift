75a894ef211d68cc9d19282a41f1f28af41b0d07
nguyen.manh.tuanb <nguyen.manh.tuanb@framgias-Mac-mini.local>
2019-02-28 14:33:30 +0700

    first

---
+++ LocationService/LocationService/LocationService.swift
@@ -0,0 +1,55 @@
+//
+//  LocationService.swift
+//  LocationService
+//
+//  Created by nguyen.manh.tuanb on 28/02/2019.
+//  Copyright © 2019 nguyen.manh.tuanb. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import RxSwift
+
+open class LocationService: NSObject, CLLocationManagerDelegate {
+    
+    public let didReceiveLocation = PublishSubject<CLLocation>()
+    private var locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        setup()
+    }
+    
+    private func setup() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    open func locationServiceEnable() -> Bool {
+        if CLLocationManager.locationServicesEnabled() {
+            switch CLLocationManager.authorizationStatus() {
+            case .notDetermined, .restricted, .denied:
+                return false
+            case .authorizedAlways, .authorizedWhenInUse:
+                return true
+            }
+        }
+        return true
+    }
+    
+    open func updateLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        locationManager.stopUpdatingLocation()
+        didReceiveLocation.onNext(location)
+    }
+    
+    private func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("map \(error.localizedDescription)")
+        didReceiveLocation.onError(error)
+    }
+}
---
+++ LocationService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationService
+//
+//  Created by nguyen.manh.tuanb on 28/02/2019.
+//  Copyright © 2019 nguyen.manh.tuanb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
